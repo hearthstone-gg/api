8cff632f9ce64e6b6e76d4b5ff52d51e7bab6ec2
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2015-05-03 16:03:31 -0400

    first

---
+++ index.js
@@ -0,0 +1,19 @@
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+
+server.connection({
+	port: 3000
+});
+
+server.start(function() {
+	console.log('Server running at:', server.info.uri);
+});
+
+server.route({
+	method: 'GET',
+	path: '/ping',
+	handler: function(request, reply) {
+		reply({ok: true});
+	}
+});
