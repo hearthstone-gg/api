acd71ae5b83a1b6ce7def5b56e93f371b541a908
Aaron Tidwell <Aaron.Tidwell@gmail.com>
2015-09-07 22:21:51 -0400

    switch to node-restful

--- index.js
+++ index.js
@@ -4,167 +4,60 @@ var argv = require('yargs').argv;
 var Hapi = require('hapi');
 var conf = require('hs.gg-config').get(argv.env || 'local');
 var config = conf.services.api;
-var server = new Hapi.Server();
 
-server.connection({
-	port: config.port,
-	routes: {
-		cors: true
-	}
+var express = require('express'),
+	bodyParser = require('body-parser'),
+	methodOverride = require('method-override'),
+	morgan = require('morgan'),
+	restful = require('node-restful'),
+	mongoose = restful.mongoose;
+var app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({
+	'extended': 'true'
+}));
+//enable CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Expose-Headers", "X-Total-Count");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  next();
 });
+app.use(bodyParser.json());
+app.use(bodyParser.json({
+	type: 'application/vnd.api+json'
+}));
+app.use(methodOverride());
 
-server.start(function() {
-	console.log('Server running at:', server.info.uri);
-});
-
-server.route({
-	config: {
-		cors: true
-	},
-	method: 'GET',
-	path: '/ping',
-	handler: function(request, reply) {
-		reply({
-			ok: true
-		});
-	}
-});
-
-var Mongoose = require('mongoose');
-
-var mongoose = Mongoose.connect(conf.services.db);
+mongoose.connect(conf.services.db);
 
 var User = conf.models.user(mongoose);
 var Game = conf.models.game(mongoose);
 
-server.route({
-	method: 'GET',
-	path: '/user',
-	handler: function(request, reply) {
-		User.find({}, function(err, users) {
-			reply(users)
-		});
-	}
-});
+var UserResource = restful.model('user', User);
+UserResource.methods(['get', 'post', 'put', 'delete']);
 
-server.route({
-	method: 'GET',
-	path: '/user/{userId}',
-	handler: function(request, reply) {
-		User.findOne({
-			'_id': request.params.userId
-		}, function(err, user) {
-			reply(user);
-		});
-	}
-});
+var GameResource = restful.model('game', Game);
+GameResource.methods(['get', 'post', 'put', 'delete']);
 
-server.route({
-	method: 'POST',
-	path: '/user',
-	handler: function(request, reply) {
-		var user = new User(request.payload);
-		user.save(function(err, user) {
-			reply(user);
-		});
-	}
+UserResource.after('get', function(req, res, next) {
+	UserResource.find({}, function(err, data){
+		res.header('X-Total-Count', data.length);
+		next();
+	});
 });
-
-server.route({
-	method: 'PUT',
-	path: '/user/{userId}',
-	handler: function(request, reply) {
-		User.findOne({
-			'_id': request.params.userId
-		}, function(err, user) {
-			for (var prop in request.payload) {
-				if (request.payload.hasOwnProperty(prop)) {
-					user[prop] = request.payload[prop];
-				}
-			}
-			user.save(function(err, user) {
-				reply(user);
-			});
-		});
-	}
-});
-
-server.route({
-	method: 'DELETE',
-	path: '/user/{userId}',
-	handler: function(request, reply) {
-		User.remove({
-			'_id': request.params.userId
-		}, function(err, user) {
-			reply({ message: 'User Deleted' });
-		});
-	}
+GameResource.after('get', function(req, res, next) {
+	GameResource.find({}, function(err, data){
+		res.header('X-Total-Count', data.length);
+		next();
+	});
 });
 
 
+UserResource.register(app, '/user');
+GameResource.register(app, '/game');
 
-/* game */
 
-server.route({
-	method: 'GET',
-	path: '/game',
-	handler: function(request, reply) {
-		Game.find({}, function(err, games) {
-			reply(games)
-		});
-	}
-});
-
-server.route({
-	method: 'GET',
-	path: '/game/{gameId}',
-	handler: function(request, reply) {
-		Game.findOne({
-			'_id': request.params.gameId
-		}, function(err, game) {
-			reply(game);
-		});
-	}
-});
-
-server.route({
-	method: 'POST',
-	path: '/game',
-	handler: function(request, reply) {
-		var game = new Game(request.payload);
-		game.save(function(err, game) {
-			reply(game);
-		});
-	}
-});
-
-server.route({
-	method: 'PUT',
-	path: '/game/{gameId}',
-	handler: function(request, reply) {
-		Game.findOne({
-			'_id': request.params.gameId
-		}, function(err, game) {
-			for (var prop in request.payload) {
-				if (request.payload.hasOwnProperty(prop)) {
-					game[prop] = request.payload[prop];
-				}
-			}
-			game.save(function(err, game) {
-				reply(game);
-			});
-		});
-	}
-});
-
-server.route({
-	method: 'DELETE',
-	path: '/game/{gameId}',
-	handler: function(request, reply) {
-		Game.remove({
-			'_id': request.params.gameId
-		}, function(err, game) {
-			reply({ message: 'Game Deleted' });
-		});
-	}
-});
+app.listen(config.port);
